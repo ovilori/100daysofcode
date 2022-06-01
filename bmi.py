433eb8096ce3272d13b7aa59f6c10c12117bab83
Olamide Ilori <olailori01@gmail.com>
2022-06-01 04:57:28 +0100

    Added BMI calculation and interpretation file

---
+++ bmi.py
@@ -0,0 +1,27 @@
+'''This program interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+The interpretation of their BMI based on the BMI value:
+
+Under 18.5 they are underweight
+Over 18.5 but below 25 they have a normal weight
+Over 25 but below 30 they are slightly overweight
+Over 30 but below 35 they are obese
+Above 35 they are clinically obese. '''
+
+#code below 
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: ")) 
+
+BMI = round((weight / (height * height)))
+
+if BMI < 18.5:
+    print(f'Your BMI is {BMI}, you are underweight.')
+elif BMI < 25:
+    print(f'Your BMI is {BMI}, you have a normal weight.')
+elif BMI < 30:
+    print(f'Your BMI is {BMI}, you are slightly overweight.')
+elif BMI < 35:
+    print(f'Your BMI is {BMI}, you are obsese.')
+else:
+    print(f'Your BMI is {BMI}, you are clinically obese.')
